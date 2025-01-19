29332478f8c4aa7d9f41d8fc2b5126be49037a35
fedorov0av <fedorov0av@gmail.com>
2025-01-19 20:51:13 +0300

    fix bugs

---
+++ html_utils/html_exceptions.py
@@ -0,0 +1,13 @@
+class ParsingException(Exception):
+    pass
+
+class InvalidHTML(ParsingException):
+    pass
+
+class NotAllowedTag(ParsingException):
+    pass
+
+class RetryAfterError(Exception):
+    def __init__(self, retry_after: int):
+        self.retry_after = retry_after
+        super().__init__(f'Flood control exceeded. Retry in {retry_after} seconds')
